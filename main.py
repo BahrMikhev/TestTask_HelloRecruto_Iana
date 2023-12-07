a91d6b246f3869a1522ab3c7d37ab483948f6acb
BahrMikhev <BahrMikhev@users.noreply.github.com>
2023-12-07 05:35:56 +0300

    Separated HTML from code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import main Flask class and request object
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 # create the Flask app
 app = Flask(__name__)
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def query_example():
-    name = request.args.get('name')
-    message = request.args.get('message')
-    return f'<h1>Hello {name}! {message}</h1>'
+    name = request.args.get('name') or ''
+    message = request.args.get('message') or ''
+    return render_template('index.html', name=name, message=message)
 
-
+# local debug environment
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
